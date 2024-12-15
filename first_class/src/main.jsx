b7a26c7c6abce4a8314bd1308d4af3282fd8d7d1
Adil Khosa <muhammadadilmemon79@gmail.com>
2024-12-15 20:43:05 +0500

    React Class 01 Complete

---
+++ first_class/src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const rootDiv = document.getElementById("root");
+
+ReactDOM.createRoot(rootDiv).render(  
+  <div>
+    <h1>Hello React 5</h1>
+    <h1>Hello React 10</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat eos enim voluptates autem consectetur officiis, commodi velit quaerat hic maiores minima porro eius architecto quas! Repellat illum, cupiditate ipsum similique dolores necessitatibus eligendi nulla odio consequatur laudantium distinctio, alias cum odit veritatis, sit voluptate adipisci perferendis repudiandae quos suscipit animi? Et excepturi incidunt alias laborum similique dolorum est quaerat sequi unde doloribus? Totam ad quisquam tenetur quo neque, voluptates quibusdam dolore earum maxime illo aliquid similique, vero dignissimos fuga magni, necessitatibus exercitationem placeat itaque. Consequatur sed repudiandae nobis maiores, amet, eius quaerat harum odit consectetur optio delectus pariatur quisquam fugit libero fuga tempore vero reprehenderit. Perspiciatis distinctio corrupti non molestiae minus dolore numquam reprehenderit a. Vitae quam voluptatem sapiente vel aliquid iste eligendi libero id corporis esse. Ex doloremque labore non necessitatibus voluptates quaerat quia, dolor ipsa distinctio voluptatem eum, vero iusto vitae. Veniam aliquid a asperiores, consequatur rem iure vel autem officiis nam quos neque corrupti dicta repudiandae, tenetur excepturi blanditiis eum quaerat magnam. Natus atque perferendis harum dolor officia hic soluta architecto nemo saepe illum at ab fuga inventore, sunt commodi maxime, odio et delectus iure libero tempora. Doloribus, rem quia pariatur accusamus aut nobis possimus doloremque quas!</p>
+    <img src="/public/vite.svg" alt="" />
+    <img src="/src/assets/react.svg" alt="" />
+    <br/>
+    <button>Click me</button>
+  </div>
+);
